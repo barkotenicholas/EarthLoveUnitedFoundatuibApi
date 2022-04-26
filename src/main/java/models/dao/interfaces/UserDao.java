2985b8617502edb7c97ca170ab86ac4a4b7516e2
marvin <marnyanjui@gmail.com>
2022-04-26 12:06:07 +0300

    Added UserDao

---
+++ src/main/java/models/dao/interfaces/UserDao.java
@@ -0,0 +1,15 @@
+package models.dao.interfaces;
+
+import models.pojos.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void add(User user);
+    List<User> getAllUsers();
+    void deleteById(int id);
+    void authenticate(String email, String password);
+    User findUserById(int id);
+    void updateUser(int id,String name, String email, String password);
+}
